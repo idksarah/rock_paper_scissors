1bc03d353a516a7293f3fb07227dcb02561639eb
idksarah <sarah.p.ngai@gmail.com>
2024-02-15 23:07:36 -0800

    Add working buttons and scoreboard

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max); //idk bout this one chief
 }
 
 let computerResponse, userResponse;
@@ -16,49 +16,37 @@ function getComputerResponse() {
   return(computerResponse);
 }
 
-/*function getUserResponse() {
-  userResponse = prompt("Rock, paper, or scissors?");
-  userResponse = userResponse.toLowerCase();
-  if (userResponse == "rock" || userResponse == "paper" || userResponse == "scissors") {
-  } else {
-    alert("Please choose one of the following:");
-    getUserResponse();
-  }
-  return(userResponse);
-}*/
-
 let userWin;
 
-function getRoundWinner(userResponse, computerResponse, ) {
+function getRoundWinner(userResponse, computerResponse/* not needed?*/) {
   computerResponse = getComputerResponse();
-  userResponse = userResponse.toLowerCase();
-  if (computerResponse == userResponse) {
+  userResponse = userResponse.toLowerCase(); //unnecessary
+  if (computerResponse == userResponse) { //strictly equal?
     alert("Tie! Try again.");
-    getRoundWinner(); 
+    return; //does this end the function??
   } else if (computerResponse == "rock") {
     if (userResponse == "paper") {
       userWin = true; 
-      alert("You Win! Paper beats Rock")
-    }
-    else {
+      alert("You Win! Paper beats Rock");
+    } else {
       userWin = false;
-      alert("You Lose! Rock beats Scissors") 
+      alert("You Lose! Rock beats Scissors"); 
     }
   } else if (computerResponse == "paper") {
     if (userResponse == "scissors") {
       userWin = true;
-      alert("You Win! Scissors beats Paper")
+      alert("You Win! Scissors beats Paper");
     } else {
       userWin = false;
-      alert("You Lose! Paper beats Rock")
+      alert("You Lose! Paper beats Rock");
     }
   } else {
     if (userResponse == "rock") {
       userWin = true;
-      alert("You Win! Rock beats Scissors")
+      alert("You Win! Rock beats Scissors");
     } else {
       userWin = false;
-      alert("You Lose! Scissors beats Paper")
+      alert("You Lose! Scissors beats Paper");
     }
   }
   return userWin;
@@ -91,7 +79,8 @@ function getRoundWinner(userResponse, computerResponse, ) {
   }
 }*/
 
-let body = document.querySelector("content");
+let body = document.querySelector("body");
+let buttons = document.querySelector(".buttons");
 
 let rock = document.createElement("button");
 let paper = document.createElement("button");
@@ -99,20 +88,46 @@ let scissors = document.createElement("button");
 
 rock.textContent = "rock!!!";
 paper.textContent = "paper :33333";
-scissors.textContent = "SCISSORS."
+scissors.textContent = "SCISSORS.";
 
-body.appendChild(rock);
-body.appendChild(paper);
-body.appendChild(scissors);
+buttons.appendChild(rock);
+buttons.appendChild(paper);
+buttons.appendChild(scissors);
 
-rock.addEventListener("click", function(){
-  getRoundWinner("rock");
-}); //idk if we need the parentheses ;sehre pooopoooo
+let userPoints = 0;
+let computerPoints = 0;
+let userResults = document.createElement("p");
+let computerResults = document.createElement("p");
 
-paper.addEventListener("click", function(){
-  getRoundWinner("paper");
-})
+let results = document.querySelector(".results");
 
-scissors.addEventListener("click", function(){
-  getRoundWinner("scissors");
-})
+function updatePoints () {
+  userResults.textContent = "User Points: " + userPoints;
+  computerResults.textContent = "Computer Points: " + computerPoints;
+}
+
+results.appendChild(userResults);
+results.appendChild(computerResults);
+
+buttons.addEventListener("click", function() {
+  if(rock.click) {
+    if(getRoundWinner("rock")) {
+      userPoints++;
+    }else {
+      computerPoints++;
+    }
+  }else if(paper.click) {
+    if(getRoundWinner("paper")) {
+      userPoints++;
+    }else {
+      computerPoints++;
+    }
+  }else {
+    if(getRoundWinner("scissors")) {
+      userPoints++;
+    }else {
+      computerPoints++;
+    }
+  }
+  updatePoints();
+});
